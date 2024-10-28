7c5ac50310246f363ecc16334ace230f5d3eec67
CasperDkk <daniel.kamau8@gmail.com>
2024-10-28 21:08:38 +0300

    basc stuff

---
+++ controlFlow.dart
@@ -0,0 +1,23 @@
+// //Control flow in dart
+// void main() {
+//   var age = 10;
+
+//   if (age > 18) {
+//     print("🗳️ Mariam is a voter in Kenya!");
+//   }
+
+//   print("👧 Mariam is still too young to vote.");
+// }
+
+//If else statements
+void main() {
+  var age = 10;
+
+  if (age > 18) {
+    print("🗳️ Mariam can vote in Kenya!");
+  } else if (age == 18) {
+    print("🎉 Mariam is just old enough to vote in Kenya!");
+  } else {
+     print("👧 Mariam is still too young to vote.");
+  }
+}
